40997bad6a29f54707bfe7eaf163b5d6cff8a3d6
icepluto <18792689010@163.com>
2019-06-17 00:40:08 +0800

    detail animation

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+<div>
+	<router-link to="/" tag="div" class="header-back" v-show="showAbs">
+    <div class="iconfont header-icon">&#xe658;</div>
+	</router-link>
+  <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+    <router-link to='/' tag="div">
+			<div class="header-fixed-left iconfont">&#xe658;</div>
+		</router-link>
+		详情选择
+	</div>
+  <div class="content"></div>
+</div>
+</template>
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+      console.log(document.documentElement.scrollTop)
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+  .header-back
+    position absolute
+    width 2rem
+    height 2rem
+    border-radius 50%
+    background-color black
+    text-align center
+    line-height 2rem
+    opacity 0.5
+    top 0
+    left 0
+    margin .5rem
+    .header-icon
+      color #ffffff
+      font-size 2rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    background-color $bgcolor
+    width 100%
+    height 2rem
+    line-height 2rem
+    text-align center
+    color #fff
+		.header-fixed-left
+			width 3rem
+			position absolute
+			top 0
+			color #fff
+      font-size 10rem
+  .content
+    width 100%
+    height 100rem
+    background-color #221341
+</style>
